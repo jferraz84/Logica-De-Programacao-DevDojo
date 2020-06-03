6d5d4477ad27ba0b9543cd1eabd46ffddbef52d4
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-06-03 10:53:22 -0300

    Aula 1 - Primeiro código, 'VARIÁVEIS'

---
+++ src/CalcularDiferencaIdade.java
@@ -0,0 +1,10 @@
+public class CalcularDiferencaIdade {
+    public static void main(String[] args) {
+
+        int firstAge = 75;
+        int secondAge = 40;
+        int ageDifference= firstAge - secondAge;
+
+        System.out.println("A diferença de idade é " + ageDifference);
+    }
+}
