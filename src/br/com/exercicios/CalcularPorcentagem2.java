7d5d4433025e866fe692aa2571520f28ee15aec9
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-06-03 15:40:15 -0300

    Aula 3 - Trabalhando com variaveis 'double' e 'float'

---
+++ src/br/com/exercicios/CalcularPorcentagem2.java
@@ -0,0 +1,24 @@
+package br.com.exercicios;
+
+public class CalcularPorcentagem2 {
+    public static void main(String[] args) {
+        
+        double salary = 5000.00;
+        double result = salary * 0.3;
+
+        System.out.println("O salário é de " + salary);
+        System.out.println("30% do sálario é: " +result);
+
+        result = salary * 0.15;
+        System.out.println("15% do sálario é: " +result);
+
+        result = salary * 0.05;
+        System.out.println("5% do sálario é: " +result);
+
+
+
+
+        
+        
+    }
+}
