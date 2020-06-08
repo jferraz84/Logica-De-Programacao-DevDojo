4d9e11e828779aabbc7ca08c2a23c758e60e1cd9
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-06-08 10:32:28 -0300

    Aula 21 - Iteração por loop 'DO-WHILE' parte 5

---
+++ src/devdojo/lacosderepeticao/whiles/Aula5DoWhile.java
@@ -0,0 +1,26 @@
+package devdojo.lacosderepeticao.whiles;
+
+import java.util.Scanner;
+
+public class Aula5DoWhile {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int desejaContinuar = 1;
+        do {
+            System.out.print("PLAYER 1: Digite um numero de 1 a 10: ");
+            int numero1 = sc.nextInt();
+            System.out.print("PLAYER 2: Digite um numero de 1 a 10: ");
+            int numero2 = sc.nextInt();
+
+            System.out.println("Acertou " +(numero1 == numero2));
+            System.out.println("Deseja continuar jogando ? ");
+            System.out.println("1: Sim");
+            System.out.println("2: Não");
+            System.out.print("R: ");
+            desejaContinuar = sc.nextInt();
+        } while (desejaContinuar == 1);
+        System.out.println("FIM do Jogo");
+
+    }
+}
